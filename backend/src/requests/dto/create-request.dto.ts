70c492aef8347cd92fb0d66f6d6fe0dbce68d111
Frankie <110301698+FrankieMaciel@users.noreply.github.com>
2025-06-03 21:16:45 -0300

    feat: Adding validation with decorators
    Using class validator to add validation to DTO files

--- backend/src/requests/dto/create-request.dto.ts
+++ backend/src/requests/dto/create-request.dto.ts
@@ -1,8 +1,24 @@
+import {
+    IsOptional,
+    IsString,
+    MinLength,
+    IsNumber
+  } from 'class-validator';
+
 export class CreateRequestDto {
+    @IsNumber()
     requestTypeId: number;
+
+    @IsString()
     adress: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
     applicantName: string;
     applicantCpf: string;
+
+    @IsString()
+    @IsOptional()
     status: string;
 }
