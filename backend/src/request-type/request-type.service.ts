93bfe3a8f6baf62e2b562106c8e7158ac2e67198
Frankie <110301698+FrankieMaciel@users.noreply.github.com>
2025-06-06 18:03:03 -0300

    fix: solving problem with cors

--- backend/src/request-type/request-type.service.ts
+++ backend/src/request-type/request-type.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRequestTypeDto } from './dto/create-request-type.dto';
 import { UpdateRequestTypeDto } from './dto/update-request-type.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class RequestTypeService {
